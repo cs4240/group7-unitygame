3fd4b03253cdc1ce3fee7a972924e199289f474a
LWWenz <leongww7@gmail.com>
2019-03-26 18:45:55 +0800

    Electron will now be able to orbit the center from any position Next: Make the electrons orbit distance the same

--- group7-unityproject/Assets/Scripts/Atom/ElectronScript.cs
+++ group7-unityproject/Assets/Scripts/Atom/ElectronScript.cs
@@ -7,20 +7,19 @@ public class ElectronScript : MonoBehaviour
     public GameObject atomBase;
     public float speed;
 
-    private Vector3 up;
+    private Vector3 updatedUp;
     // Start is called before the first frame update
     void Start()
     {
         Vector3 front, right;
         front = transform.position - atomBase.transform.position;
-        front = front.Normalize;
-        right = Vector3.Cross(front.Normalize, new Vector3(0, 1, 0));
-        up = Vector3.Cross(right, front);
+        right = Vector3.Cross(front.normalized, new Vector3(0, 1, 0));
+        updatedUp = Vector3.Cross(right, front);
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.RotateAround(atomBase.transform.position, atomBase.transform.up, speed * Time.deltaTime);
+        transform.RotateAround(atomBase.transform.position, updatedUp, speed * Time.deltaTime);
     }
 }
