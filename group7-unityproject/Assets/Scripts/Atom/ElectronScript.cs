737bfcc5be14e02c9f25263c6786c4ca7a9133ab
LWWenz <leongww7@gmail.com>
2019-03-26 17:17:47 +0800

    Hydrogen Atom v1.0

---
+++ group7-unityproject/Assets/Scripts/Atom/ElectronScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ElectronScript : MonoBehaviour
+{
+    public GameObject atomBase;
+    public float speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.RotateAround(atomBase.transform.position, atomBase.transform.up, speed * Time.deltaTime);
+    }
+}
