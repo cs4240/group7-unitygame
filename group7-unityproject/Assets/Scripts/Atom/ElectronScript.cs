9bfe82dc8ddcf254b7d23e1831594853b5d41496
LWWenz <leongww7@gmail.com>
2019-03-26 19:42:07 +0800

    After image of electron done

--- group7-unityproject/Assets/Scripts/Atom/ElectronScript.cs
+++ group7-unityproject/Assets/Scripts/Atom/ElectronScript.cs
@@ -11,10 +11,13 @@ public class ElectronScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        //To get the new up from the new front distance
         Vector3 front, right;
         front = transform.position - atomBase.transform.position;
         right = Vector3.Cross(front.normalized, new Vector3(0, 1, 0));
         updatedUp = Vector3.Cross(right, front);
+
+
     }
 
     // Update is called once per frame
