6011390001f6d4d41d111dfeb8c61047daba80c0
LWWenz <leongww7@gmail.com>
2019-03-26 17:43:24 +0800

    WIP: Electrons to get the Up vector between itself and center of atom to rotate around it

--- group7-unityproject/Assets/Scripts/Atom/ElectronScript.cs
+++ group7-unityproject/Assets/Scripts/Atom/ElectronScript.cs
@@ -6,10 +6,16 @@ public class ElectronScript : MonoBehaviour
 {
     public GameObject atomBase;
     public float speed;
+
+    private Vector3 up;
     // Start is called before the first frame update
     void Start()
     {
-        
+        Vector3 front, right;
+        front = transform.position - atomBase.transform.position;
+        front = front.Normalize;
+        right = Vector3.Cross(front.Normalize, new Vector3(0, 1, 0));
+        up = Vector3.Cross(right, front);
     }
 
     // Update is called once per frame
