f1d8809a225209d99a855fc1a606f4ef076ee30b
LWWenz <leongww7@gmail.com>
2019-03-27 14:14:20 +0800

    Work Done: Setting distance of atom to the center of atomBase | Check for Bugs: If in an angled, will it be still revolving correctly?

--- group7-unityproject/Assets/Scripts/Atom/ElectronScript.cs
+++ group7-unityproject/Assets/Scripts/Atom/ElectronScript.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class ElectronScript : MonoBehaviour
 {
     public GameObject atomBase;
-    public float speed;
+    public float speed, distanceFromCenter;
 
     private Vector3 updatedUp;
     // Start is called before the first frame update
@@ -17,7 +17,8 @@ public class ElectronScript : MonoBehaviour
         right = Vector3.Cross(front.normalized, new Vector3(0, 1, 0));
         updatedUp = Vector3.Cross(right, front);
 
-
+        //To set the distance from the center
+        transform.position = atomBase.transform.position + distanceFromCenter * front.normalized;
     }
 
     // Update is called once per frame
